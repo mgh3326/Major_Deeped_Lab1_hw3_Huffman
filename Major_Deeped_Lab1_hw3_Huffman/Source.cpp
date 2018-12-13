28e1ad81213c81010594c55eb403ad748874dbea
mgh3326 <mgh3326@naver.com>
2018-12-13 14:31:44 +0900

    Update is Euqual

--- Major_Deeped_Lab1_hw3_Huffman/Source.cpp
+++ Major_Deeped_Lab1_hw3_Huffman/Source.cpp
@@ -26,48 +26,12 @@ unsigned char** mem_alloc2_d(int n_height, int n_width, unsigned char nlnit_val)
 	}
 	return rtn;
 }
-unsigned int bitsToBytes(unsigned char *bits)
-{
-	unsigned int sum = 0;
-	for (int i = 0; i < 8; i++)
-	{
-		sum += bits[i] - '0';
-		sum <<= 1;
-	}
-	return sum;
 
-}
 
-template<typename T>
-void pop_front(std::vector<T>& vec)
-{
-	assert(!vec.empty());
-	vec.erase(vec.begin());
-}
-void test(vector<int> myvector[]) {
-	for (int i = 0; i < 256; i++)
-	{
-		for (int j = 0; j < myvector[i].size(); j++)
-		{
-			if (j == 0)
-			{
-				std::cout << i << " : ";
-			}
-			std::cout << myvector[i][j];
-			if (j == myvector[i].size() - 1)
-				std::cout << endl;
-
-		}
-	}
-	printf("test\n");
-
-}
 void histogram_generate(unsigned char **in, int * hist)
 {
 
-	//double hist_cdf[256] = { 0.0 };//HistogramEqualization 을 담을 배열입니다.
-	//unsigned char** eq_img = mem_alloc2_d(X_MAX, Y_MAX, 0);
-	//double histogram_sum = 0;
+
 	for (int i = 0; i < 256; i++)
 		hist[i] = 0;//initialze
 	for (int h = 0; h < Y_MAX; h++)
@@ -77,31 +41,14 @@ void histogram_generate(unsigned char **in, int * hist)
 			hist[in[h][w]]++;//히스토 그램을 만듭니다.
 		}
 	}
-	//for (int i = 0; i < 256; i++)
-	//{
-	//	histogram_sum = histogram_sum + hist[i];// 히스토그램의 누적 합을 [0,1]로 정규화 한 cdf를 구한다.
-	//	hist_cdf[i] = histogram_sum / (Y_MAX *X_MAX);//HistogramEqualization을 실행합니다.
-	//}
-	//for (int h = 0; h < Y_MAX; h++)
-	//{
-	//	for (int w = 0; w < X_MAX; w++)
-	//	{//히스토그램의 누적 합을 [0,1]로 정규화 한 cdf를 구한다.
-	//		eq_img[h][w] = hist_cdf[in[h][w]] * 255;//히스템그램을 이미지로 만듭니다.
-	//	}
-	//}
-	//string output = "Histogram_Equalization";//결과를 raw 이미지로 출력합니다.
-	//output.append(".raw");
-	//FILE* outfile = fopen(output.c_str(), "w+b");
-	//for (int i = 0; i < Y_MAX; i++)
-	//{
-	//	fwrite(eq_img[i], sizeof(char), X_MAX, outfile);
-	//}
-	//fclose(outfile);
+
 }
 unsigned char extract(unsigned char data, int i) {
 	return (data >> i) & 1;
 }
 int encode() {
+	printf("encode start\n");
+
 	unsigned char** in_data = mem_alloc2_d(X_MAX, Y_MAX, 0);
 	// 이미지를 읽어온다
 
@@ -118,21 +65,6 @@ int encode() {
 	fclose(in);
 	int *hist = new int[256];//histogram을 받을 배열입니다.
 	histogram_generate(in_data, hist);//histogram을 만듭니다.
-	//int num = 0;
-	//for (int i = 0; i < 256; i++)
-	//{
-	//	if (hist[i] != 0)
-	//	{
-	//		printf("%d : %d\n", i, hist[i]);
-	//		num++;
-	//	}
-
-	//}
-	printf("Complete!\n");
-	//unsigned char arr[] = {10,100,150,250,1,2};
-	//int freq[] = { 10,20,30,40,1,2};
-	//int size = sizeof(arr) / sizeof(arr[0]);
-
 	unsigned char* arr = new unsigned char[256];//넉넉하게 256 을 할당해줘도 문제가 없을까?
 	int* freq = new int[256];
 	int size = 0;
@@ -144,7 +76,6 @@ int encode() {
 			size++;
 		}
 	}
-	int N = 256;
 	vector<int> adj[256];
 
 	HuffmanCodes(arr, freq, size, adj);
@@ -200,27 +131,25 @@ int encode() {
 
 	for (int i = 0; i < saved_vector.size() % 8; i++)
 	{
-		b0[i] = saved_vector[i + saved_vector.size() - 8];
+		b0[i] = saved_vector[i + saved_vector.size() - saved_vector.size() % 8];
 
 	}
 	for (int i = 0; i < 8 - saved_vector.size() % 8; i++)
 	{
-		b0[i] = 0;
 
+		b0[(saved_vector.size() % 8) + i] = 0;
 	}
 	unsigned char  a = b0.to_ulong();
 	fwrite(&a, sizeof(unsigned char), 1, huf_out);
 
 	fclose(huf_out);
-	std::cout << test_count << endl;
-
+	printf("encode end\n");
 	return 0;
 }
 int decode() {
-
+	printf("decode start\n");
 	string filePath = "huffman_table.txt";
 	map< string, int > m;
-
 	// read File
 	vector<int> adj[256];
 	int open_index = 0;
@@ -232,16 +161,12 @@ int decode() {
 			{
 				for (int i = 0; i < line.size(); i++)
 				{
-					adj[open_index].push_back(line[i]-48);
+					adj[open_index].push_back(line[i] - 48);
 					m.insert(make_pair(line, open_index));
-
-
 				}
 
 			}
 			open_index++;
-
-
 		}
 		openFile.close();
 	}
@@ -252,7 +177,6 @@ int decode() {
 		return 0;
 	}
 	unsigned char test;
-	//saved_vector.clear();//초기화 해보고 다시 가보자
 	vector<int> saved_vector;//이걸 array로 하면 빨라서 좋을거 같기도하다.
 
 	char bit[8];
@@ -266,11 +190,7 @@ int decode() {
 
 	fclose(huf_in);
 
-
-
-	//saved vector를 write 해야하고
-	//saved vector처럼 써야하네
-	vector<int> temp_vector;//이진법화 하는게 잘 안되네
+	vector<int> temp_vector;
 
 	int count = 0;
 	bool oh = true;
@@ -288,62 +208,74 @@ int decode() {
 		iter = m.find(temp_string);
 		if (iter != m.end())
 		{
-			out_data[count / 256][count % 256] = iter->second;
+			out_data[count / 256][count % 256] = iter->second;//out_data에 담기
 
 			count++;
 			temp_string = "";
 		}
-
-
-		/*cout << "test";*/
-
-
-		/*pop_front(saved_vector);*/
-		//for (int i = 0; i < 256; i++)
-		//{
-		//	if (adj[i].size() == 0)//continue
-		//		continue;
-		//	if (temp_vector.size() == adj[i].size())
-		//	{
-		//		for (int j = 0; j < temp_vector.size(); j++)
-		//		{
-
-		//			if (temp_vector[j] != adj[i][j])
-		//			{
-		//				break;
-		//			}
-		//			if (j == temp_vector.size() - 1)//마지막꺼 까지 같을때
-		//			{
-		//				out_data[count / 256][count % 256] = i;
-
-		//				count++;
-		//				temp_vector.clear();
-		//				oh = false;//2중 for문 탈출
-		//				break;
-		//			}
-		//		}
-		//		if (oh == false)
-		//		{
-		//			oh = true;
-		//			break;
-		//		}
-		//	}
-		//}
 	}
-	std::cout << count << endl;
 	FILE* outfile = fopen("output.raw", "w+b");
 
 	for (int i = 0; i < 256; i++)
 	{
 		fwrite(out_data[i], sizeof(unsigned char), 256, outfile);
 	}
+	printf("decode end\n");
+	fclose(outfile);
+	return 0;
+}
+int is_equal() {
+	cout << "is_equal Start" << endl;
+
+	unsigned char** in_data = mem_alloc2_d(X_MAX, Y_MAX, 0);
+	// 이미지를 읽어온다
+	unsigned char** out_data = mem_alloc2_d(X_MAX, Y_MAX, 0);
+	// 이미지를 읽어온다
+
+	FILE* in = fopen("lena256.raw", "rb");//file input
+	if (in == NULL)
+	{
+		printf("File not found!!\n");
+		return 0;
+	}
+	for (int i = 0; i < Y_MAX; i++)
+	{
+		fread(in_data[i], sizeof(char), X_MAX, in);
+	}
+	fclose(in);
+
+
+
+
+	FILE* in_out = fopen("output.raw", "rb");//file input
+	if (in == NULL)
+	{
+		printf("File not found!!\n");
+		return 0;
+	}
+	for (int i = 0; i < Y_MAX; i++)
+	{
+		fread(out_data[i], sizeof(char), X_MAX, in_out);
+	}
+	fclose(in_out);
+	for (int h = 0; h < Y_MAX; h++)
+	{
+		for (int w = 0; w < X_MAX; w++)
+		{
+			if (in_data[h][w] != out_data[h][w])
+			{
+				cout << "[" << h << "][" << w << "] 의 값이 달라버립니다." << endl;
+			}
+		}
+	}
+	cout << "is_equal End" << endl;
 	return 0;
 }
 int main(void)
 {
-	printf("encode start\n");
+
 	encode();
-	printf("decode start\n");
 	decode();
+	is_equal();
 	return 0;
 }
